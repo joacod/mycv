2a6e1b2107541a1de3c7f5f2fa9c4f4fdadbf076
Joaquin Diaz <joacod@gmail.com>
2023-10-04 23:44:45 -0300

    clean up, readme and favicon

---
+++ app/credits/page.tsx
@@ -0,0 +1,19 @@
+const Credits = () => {
+  return (
+    <div>
+      <h1>Credits</h1>
+      <ul>
+        <li>
+          <a
+            href="https://www.flaticon.com/free-icons/files-and-folders"
+            title="files and folders icons"
+          >
+            Image for the favicon created by Muhammad Atif - Flaticon
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Credits;
