6ad069c48aa181d0a8bb0cd7f105d3a62ac53cf2
Joaquin Diaz <joacod@gmail.com>
2023-10-05 17:09:49 -0300

    wrapper fix

--- app/[locale]/components/Wrapper/Wrapper.tsx
+++ app/[locale]/components/Wrapper/Wrapper.tsx
@@ -4,8 +4,8 @@ import { Footer } from "../Footer/Footer";
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen bg-gradient-to-b from-blue-700 to-blue-400 ">
-      <div className="relative flex h-screen flex-col">
+    <div className="min-h-screen bg-gradient-to-b from-blue-700 to-blue-400 ">
+      <div className="relative flex min-h-screen flex-col">
         <main className="mx-20 mt-20 flex-grow shadow-2xl">
           <Navigation />
           <div id="content" className="bg-neutral-100 p-10 text-black">
