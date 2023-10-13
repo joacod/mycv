eacb226af333b91dfa166f43c686d42e9e2e1233
Joaquin Diaz <joacod@gmail.com>
2023-10-13 20:21:03 -0300

    skills wrapper refactor

--- app/[locale]/components/Skills/SkillsDesktop/SkillsDesktop.tsx
+++ app/[locale]/components/Skills/SkillsDesktop/SkillsDesktop.tsx
@@ -11,6 +11,7 @@ import {
   SiCsswizardry,
 } from "react-icons/si";
 import { BiLogoTypescript } from "react-icons/bi";
+import { SkillWrapper } from "./SkillWrapper";
 
 export const SkillsDesktop = () => {
   const t = useTranslations("Skills");
@@ -20,38 +21,38 @@ export const SkillsDesktop = () => {
       <div className="px-28 py-20">
         <h1 className="mb-14 text-5xl font-bold">{t("title")}</h1>
 
-        <div className="grid grid-rows-2 text-primary-content hover:text-secondary-content lg:grid-cols-5 lg:gap-y-5">
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <FaReact size={110} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiNextdotjs size={100} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiVite size={90} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiTailwindcss size={110} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiAmazonaws size={90} className="text-accent-content " />
-          </div>
+        <div className="grid grid-rows-2 lg:grid-cols-5 lg:gap-y-5">
+          <SkillWrapper>
+            <FaReact size={110} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiNextdotjs size={100} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiVite size={90} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiTailwindcss size={110} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiAmazonaws size={90} />
+          </SkillWrapper>
 
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiJavascript size={90} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <BiLogoTypescript size={120} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiGit size={100} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiHtml5 size={90} className="text-accent-content " />
-          </div>
-          <div className="mask mask-hexagon-2 flex h-40 w-40 origin-center transform items-center justify-center bg-primary transition-transform duration-200 hover:mask-decagon hover:scale-125 hover:bg-secondary">
-            <SiCsswizardry size={90} className="text-accent-content " />
-          </div>
+          <SkillWrapper>
+            <SiJavascript size={90} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <BiLogoTypescript size={120} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiGit size={100} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiHtml5 size={90} />
+          </SkillWrapper>
+          <SkillWrapper>
+            <SiCsswizardry size={90} />
+          </SkillWrapper>
         </div>
       </div>
     </div>
