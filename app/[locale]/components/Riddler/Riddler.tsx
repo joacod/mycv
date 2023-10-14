22c545cc518b266db8ff5629c2c4c781c078b171
Joaquin Diaz <joacod@gmail.com>
2023-10-13 22:52:12 -0300

    riddler skeleton

--- app/[locale]/components/Riddler/Riddler.tsx
+++ app/[locale]/components/Riddler/Riddler.tsx
@@ -4,8 +4,13 @@ export const Riddler = () => {
   const t = useTranslations("Riddler");
 
   return (
-    <div id="section-riddler">
-      <p>{t("title")}</p>
+    <div
+      id="section-riddler"
+      className="bg-neutral px-28 py-24 text-center text-accent"
+    >
+      <h1 className="text-9xl font-bold">{t("title")}</h1>
+      <h3 className="text-xl mt-10">{t("riddle")}</h3>
+      <h4 className="text-2xl mt-5">{t("end")}</h4>
     </div>
   );
 };
