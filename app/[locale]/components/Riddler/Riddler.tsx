046b8ab7c70c3e9c6abbcc91a6bf434e6c6fd60b
Joaquin Diaz <joacod@gmail.com>
2023-10-16 19:29:01 -0300

    question concept

--- app/[locale]/components/Riddler/Riddler.tsx
+++ app/[locale]/components/Riddler/Riddler.tsx
@@ -1,14 +1,27 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import React, { useState } from "react";
 
 export const Riddler = () => {
-  const t = useTranslations("Riddler");
+  const phrases = ["What", "do", "I", "know", "about", "you"];
+  const [phraseIndex, setPhraseIndex] = useState(0);
+
+  const handleClick = () => {
+    if (phraseIndex < phrases.length) {
+      setPhraseIndex(phraseIndex + 1);
+    } else {
+      // open modal
+    }
+  };
 
   return (
     <div
       id="section-riddler"
       className="bg-neutral px-28 py-24 text-center text-accent"
     >
-      <h1 className="text-9xl font-bold">{t("title")}</h1>
+      <h1 className="cursor-pointer text-9xl font-bold" onClick={handleClick}>
+        {phrases.slice(0, phraseIndex).join(" ")}?
+      </h1>
     </div>
   );
 };
