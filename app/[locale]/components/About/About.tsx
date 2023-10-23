c2e75d7a35c158e43c8c514e0fe31adc808b65c4
Joaquin Diaz <joacod@gmail.com>
2023-10-23 01:11:17 -0300

    local switcher refactor

--- app/[locale]/components/About/About.tsx
+++ app/[locale]/components/About/About.tsx
@@ -7,16 +7,16 @@ export const About = () => {
 
   return (
     <div className="hero min-h-screen bg-neutral text-neutral-content">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content relative flex-col lg:flex-row-reverse">
         <Image
-          className="lg:hidden"
+          className="z-0 lg:hidden"
           src="/avatar.png"
           alt="Avatar"
           width={180}
           height={196}
         />
         <Image
-          className="hidden lg:inline"
+          className="z-0 hidden lg:inline"
           src="/avatar.png"
           alt="Avatar"
           width={480}
--- app/[locale]/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ app/[locale]/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,13 +1,14 @@
 import { useLocale, useTranslations } from "next-intl";
 import Link from "next-intl/link";
 import { IoLanguageOutline } from "react-icons/io5";
+import { languages } from "@/middleware";
 
 const LocaleSwitcher = () => {
   const locale = useLocale();
   const t = useTranslations("LocaleSwitcher");
 
   return (
-    <li>
+    <li className="relative">
       <details>
         <summary>
           <span className="hidden lg:inline">
@@ -16,31 +17,13 @@ const LocaleSwitcher = () => {
           <span className="lg:hidden">{t("title")}</span>
         </summary>
         <ul className="z-50 space-y-2 bg-base-100 p-2">
-          <li>
-            <Link href={`en`} className={locale === "en" ? "active" : ""}>
-              {t("en")}
-            </Link>
-          </li>
-          <li>
-            <Link href={`es`} className={locale === "es" ? "active" : ""}>
-              {t("es")}
-            </Link>
-          </li>
-          <li>
-            <Link href={`pt`} className={locale === "pt" ? "active" : ""}>
-              {t("pt")}
-            </Link>
-          </li>
-          <li>
-            <Link href={`fr`} className={locale === "fr" ? "active" : ""}>
-              {t("fr")}
-            </Link>
-          </li>
-          <li>
-            <Link href={`de`} className={locale === "de" ? "active" : ""}>
-              {t("de")}
-            </Link>
-          </li>
+          {languages.map((lang) => (
+            <li key={lang}>
+              <Link href={lang} className={locale === lang ? "active" : ""}>
+                {t(lang)}
+              </Link>
+            </li>
+          ))}
         </ul>
       </details>
     </li>
