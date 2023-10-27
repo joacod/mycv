613e0a0c1b182eb9ed4b25253c29a99104f5b521
Joaquin Diaz <joacod@gmail.com>
2023-10-26 22:52:20 -0300

    about

--- app/[locale]/components/About/About.tsx
+++ app/[locale]/components/About/About.tsx
@@ -6,34 +6,36 @@ export const About = () => {
   const t = useTranslations("About");
 
   return (
-    <div className="hero z-0 min-h-screen bg-neutral text-neutral-content">
+    <section className="hero z-0 min-h-screen bg-neutral text-neutral-content">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image
-          className="lg:hidden"
-          src="/avatar.png"
-          alt="Avatar"
-          width={180}
-          height={196}
-        />
-        <Image
-          className="hidden lg:inline"
-          src="/avatar.png"
-          alt="Avatar"
-          width={480}
-          height={526}
-        />
-        <div>
+        <figure className="lg:hidden">
+          <Image
+            src="/avatar.png"
+            alt="Avatar of a guy with a beard, winking and smiling"
+            width={180}
+            height={196}
+          />
+        </figure>
+        <figure className="hidden lg:inline lg:w-1/5">
+          <Image
+            src="/avatar.png"
+            alt="Avatar of a guy with a beard, winking and smiling"
+            width={480}
+            height={526}
+          />
+        </figure>
+        <div className="lg:w-4/5 lg:pr-20">
           <h1 className="text-5xl font-bold">{t("title")}</h1>
           <p className="pt-6">{t("description")}</p>
           <p className="pt-6">{t("role")}</p>
-          <p className="py-6">{t("colaborate")}</p>
+          <p className="py-6">{t("collaborate")}</p>
           <div className="hidden lg:inline">
-            <a className="btn btn-primary " href="#section-experience">
+            <a className="btn btn-primary" href="#section-experience">
               {t("getstarted")} <FaHandPointDown size={22} />
             </a>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
