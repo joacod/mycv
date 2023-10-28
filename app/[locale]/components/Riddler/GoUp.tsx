05139aafd73f149d8f0df8fa3ffdb9b91d35764b
Joaquin Diaz <joacod@gmail.com>
2023-10-28 02:28:17 -0300

    go to top translations

--- app/[locale]/components/Riddler/GoUp.tsx
+++ app/[locale]/components/Riddler/GoUp.tsx
@@ -1,14 +1,18 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const GoUp = () => {
+  const t = useTranslations("Common");
+
   return (
     <section className="flex justify-center bg-neutral pb-8 lg:pb-10">
       <Link
         href="#navigation"
-        className="delay-50 text-neutral-content transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-125"
+        className="delay-50 flex flex-col items-center text-neutral-content transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-125"
       >
         <BsFillArrowUpCircleFill size={50} aria-label="Up arrow Icon" />
+        <span className="mt-2">{t("top")}</span>
       </Link>
     </section>
   );
