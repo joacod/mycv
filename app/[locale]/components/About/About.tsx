5968ed677970e7eda9b6ac6935511401308ecdf0
Joaquin Diaz <joacod@users.noreply.github.com>
2024-09-08 00:45:46 -0300

    Colors refactor (#34)
    * keywords
    
    * fix warning node_env
    
    * about and experience sections
    
    * new avatar image
    
    * font button text
    
    * navigation refactor

--- app/[locale]/components/About/About.tsx
+++ app/[locale]/components/About/About.tsx
@@ -6,29 +6,29 @@ export const About = () => {
   const t = useTranslations("About");
 
   return (
-    <section className="hero z-0 min-h-screen bg-neutral text-neutral-content">
+    <section className="hero z-0 min-h-screen bg-primary bg-gradient-to-b from-secondary to-primary text-primary-content">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <figure className="animate-flip-up lg:hidden">
           <Image
             src="/avatar.webp"
-            alt="Avatar of a guy with a beard, winking and smiling"
-            width={180}
-            height={196}
+            alt="Avatar of a guy with a beard, glasses and smiling"
+            width={250}
+            height={250}
             priority={true}
             placeholder="empty"
           />
         </figure>
-        <figure className="hidden animate-flip-up lg:inline lg:w-1/5">
+        <figure className="hidden animate-flip-up lg:inline lg:w-3/12">
           <Image
             src="/avatar.webp"
-            alt="Avatar of a guy with a beard, winking and smiling"
-            width={480}
-            height={526}
+            alt="Avatar of a guy with a beard, glasses and smiling"
+            width={500}
+            height={500}
             priority={true}
             placeholder="empty"
           />
         </figure>
-        <article className="animate-flip-up lg:w-4/5 lg:pr-20">
+        <article className="animate-flip-up lg:w-9/12 lg:pr-20">
           <h1 className="text-5xl font-bold">{t("title")}</h1>
           <p className="pt-6 text-xl font-semibold lg:pr-40 lg:text-2xl">
             {t("description")}
@@ -38,7 +38,7 @@ export const About = () => {
             <p className="pt-4 md:pt-0 lg:pt-0">{t("collaborate")}</p>
           </div>
           <div className="hidden lg:mt-10 lg:flex">
-            <a className="btn btn-primary" href="#section-experience">
+            <a className="btn btn-neutral" href="#section-experience">
               {t("getstarted")} <FaHandPointDown size={22} />
             </a>
           </div>
--- app/[locale]/components/Navigation/FontBtn.tsx
+++ app/[locale]/components/Navigation/FontBtn.tsx
@@ -5,7 +5,7 @@ import { BsFillClipboard2CheckFill } from "react-icons/bs";
 import confetti from "canvas-confetti";
 
 export const FontBtn = ({ title }: { title: string }) => {
-  const fonts = ["system-ui", "cursive", "Fira Code", "Tahoma", "Courier New"];
+  const fonts = ["system-ui", "Fira Code", "Tahoma", "Courier New"];
   const [currentFont, setCurrentFont] = useState(fonts[0]);
 
   const changeFont = () => {
@@ -40,7 +40,7 @@ export const FontBtn = ({ title }: { title: string }) => {
       onClick={() => changeFont()}
       aria-label={`${title} | Change Font randomly`}
     >
-      <span className="hidden lg:inline">
+      <span className="inline">
         <BsFillClipboard2CheckFill size={32} />
       </span>{" "}
       <span>{title}</span>
