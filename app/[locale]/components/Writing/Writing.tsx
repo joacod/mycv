f759290edf6b916c2730f49b66b8d9422dbe6fdc
Joaquin Diaz <joacod@gmail.com>
2024-09-08 11:28:25 -0300

    writing icons for mobile

--- app/[locale]/components/Writing/Writing.tsx
+++ app/[locale]/components/Writing/Writing.tsx
@@ -11,7 +11,7 @@ export const Writing = () => {
     >
       <h1 className="pb-6 text-5xl font-bold">{t("title")}</h1>
       <p className="lg:w-1/2 lg:text-center">{t("description")}</p>
-      <div className="flex justify-center gap-5 pt-2 lg:gap-20">
+      <div className="flex justify-center pt-2 lg:gap-20">
         <article>
           <a
             href="https://dev.to/joacod"
@@ -20,9 +20,10 @@ export const Writing = () => {
             aria-label={`Dev.to`}
             className="flex flex-col items-center"
           >
+            <BiLogoDevTo size={150} className="lg:hidden" />
             <BiLogoDevTo
               size={200}
-              className="delay-50 transition duration-200 ease-in-out hover:scale-125"
+              className="delay-50 hidden transition duration-200 ease-in-out hover:scale-125 lg:flex"
             />
             <p className="text-xl font-semibold">Dev.to</p>
           </a>
@@ -35,9 +36,10 @@ export const Writing = () => {
             aria-label={`Medium`}
             className="flex flex-col items-center"
           >
+            <BiLogoMediumSquare size={150} className="lg:hidden" />
             <BiLogoMediumSquare
               size={200}
-              className="delay-50 transition duration-200 ease-in-out hover:scale-125"
+              className="delay-50 hidden transition duration-200 ease-in-out hover:scale-125 lg:flex"
             />
             <p className="text-xl font-semibold">Medium</p>
           </a>
