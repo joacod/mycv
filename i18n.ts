6a7a90c308d408b951cab2a2235fd416e3f932a9
Joaquin Diaz <dev@joacod.com>
2024-11-11 02:24:29 -0300

    i18n update

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,10 @@
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./dictionaries/${locale}.json`)).default,
-}));
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+
+  return {
+    locale,
+    messages: (await import(`./dictionaries/${locale}.json`)).default,
+  };
+});
