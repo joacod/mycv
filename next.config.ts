98b3ca3e8f68435229410af90e85ffe18b87f710
Joaquin Diaz <dev@joacod.com>
2024-12-11 02:51:01 -0300

    Next 15.1 update (#49)
    * next 15.1 update
    
    * tsconfig target

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
+import { NextConfig } from "next";
+
 const withNextIntl = require("next-intl/plugin")("./i18n.ts");
-const nextConfig = withNextIntl({});
+const nextConfig: NextConfig = withNextIntl({});
 
 module.exports = nextConfig;
