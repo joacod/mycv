b35a90d01ed48e0f7410a1f2f3692e52fb3c07c2
Joaquin Diaz <dev@joacod.com>
2025-08-08 01:16:34 -0300

    next config remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,19 @@ import { NextConfig } from "next";
 const withNextIntl = require("next-intl/plugin")("./i18n/request.ts");
 const nextConfig: NextConfig = withNextIntl({
   images: {
-    domains: [
-      "media2.dev.to",
-      "res.cloudinary.com",
-      "dev-to-uploads.s3.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+      },
     ],
   },
 });
